3122a712bd9e6f05aab2f1118bff2d190fc1784e
Denison Junior <denison.junior@cebraspe.org.br>
2026-07-17 11:49:58 -0300

    feat: add asset management with Axum, including list and create endpoints

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,31 @@
-use axum::Router;
+use std::sync::Arc;
+
+use axum::{Json, Router};
+use axum::extract::State;
 use axum::routing::get;
+use serde::Deserialize;
 use tokio::net::TcpListener;
+use tokio::sync::Mutex;
 use tracing::info;
 use tracing_subscriber::fmt::format::FmtSpan;
 use tracing_subscriber::Layer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+
+use crate::models::Asset;
+
+#[derive(Clone)]
+pub struct AppState {
+    assets: Arc<Mutex<Vec<Asset>>>
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            assets: Default::default()
+        }
+    }
+}
 
 pub struct App;
 
@@ -20,7 +40,9 @@ impl App {
         info!("Starting server...");
 
         let listener = TcpListener::bind("127.0.0.1:3000").await?;
-        let router = Router::new().route("/", get(hello_world));
+        let router = Router::new()
+            .route("/", get(list_assets).post(create_asset))
+            .with_state(AppState::new());
 
 
         axum::serve(listener, router).await?;
@@ -29,7 +51,31 @@ impl App {
     }
 }
 
-#[tracing::instrument]
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+#[tracing::instrument(skip_all)]
+async fn list_assets(state: State<AppState>) -> Json<Vec<Asset>> {
+    let assets = state.assets.lock().await;
+    Json(assets.clone())
+}
+
+#[derive(Deserialize)]
+struct CreateAssetRequest {
+    name: String,
+    unit_value: f64,
+}
+
+#[tracing::instrument(skip_all)]
+async fn create_asset(state: State<AppState>, Json(request): Json<CreateAssetRequest>) -> Json<Asset> {
+    let mut assets = state.assets.lock().await;
+    
+    let id = assets.iter().map(|asset| asset.id).max().unwrap_or_default() + 1;
+    
+    let asset = Asset {
+        id,
+        name: request.name,
+        unit_value: request.unit_value,
+    };
+    
+    assets.push(asset.clone());
+
+    Json(asset)
 }
